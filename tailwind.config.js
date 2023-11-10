351165b46bd211705988d6171b950b30dd644ce3
Nephilus-notes <114515979+Nephilus-notes@users.noreply.github.com>
2023-11-10 10:24:12 -0600

    added colab colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{html,ts}"
-  ],
+  content: ["./src/**/*.{html,ts}"],
   theme: {
+    colors: {
+      "colab-teal": "#72CDAB",
+      "colab-blue": "#485BFF",
+      "colab-yellow": "#FFCC35",
+      "colab-pink": "#EFA6D2",
+      "colab-light-blue": "#5FBEFF"
+    },
     extend: {},
   },
   plugins: [],
-}
-
+};
