25b553a1d568cf4a8bdb68e4b3b6848af1d2dd24
Nephilus-notes <114515979+Nephilus-notes@users.noreply.github.com>
2023-11-12 10:52:56 -0600

    took container out of photo card

--- src/app/views/bio/bio.component.ts
+++ src/app/views/bio/bio.component.ts
@@ -8,9 +8,18 @@ import { Component } from '@angular/core';
 export class BioComponent {
 conditional: boolean = true;
 
-seattleContent: string = "I danced at PNB for a few years, performing soloist and principal roles"
-seattleImg: string = "/assets/img/Emeralds2014-Backstage_LT_078.JPG"
 controlCodePhoto: number = 1;
 controlCodeProjects: number = 2;
+seattleContent: string = "I danced at PNB for a few years, performing soloist and principal roles"
+seattleImg: string = "/assets/img/Emeralds2014-Backstage_LT_078.JPG"
+
+studentContent: string = "I studied at the School of American Ballet, performed with New York City Ballet, and finally moved to seattle to train at the Pacific Northwest Ballet"
+studentImg: string = "/assets/img/Half C-Daniil Simkin.jpg"
+
+rnzbContent: string = "I moved to new zealand to dance with the Royal New Zealand Ballet, where I performed soloist and principal roles, and took up leatherworking"
+rnzbImg: string = "/assets/img/mr_wolf_with_alayna].jpg"
+
+freelanceContent: string = "I returned to New York and freelanced for a few years"
+freelanceImg: string = "/assets/img/Read.jpg"
 
 }
