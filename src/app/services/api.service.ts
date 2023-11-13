81f3e399b3eab0b1bfd8c035d6c52f110f77cd86
Nephilus-notes <114515979+Nephilus-notes@users.noreply.github.com>
2023-11-13 13:21:12 -0600

    added getHomeArticles function to api service

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,12 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Article } from '../models/article';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 NYTimesURL: string = "https://api.nytimes.com/svc/topstories/v2/home.json?api-key=R9GzySamDkKvMtFQnDnKu5qNiZ1a0lDE"
 
+public getHomeArticles(): Observable<Article[]> {
+  const articles = this.http.get<Article[]>(this.NYTimesURL);
+  return articles;
+}
 
 }
