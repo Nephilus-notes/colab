21ca0faea33f00610e9e89a1ee20dec953e49312
Nephilus-notes <114515979+Nephilus-notes@users.noreply.github.com>
2023-11-14 13:59:39 -0600

    bugfix: api in deploy

--- src/app/views/api/api.component.ts
+++ src/app/views/api/api.component.ts
@@ -14,7 +14,9 @@ articles!: Article[];
 
   public getArticles(): void {
     this.apiService.getHomeArticles().subscribe(data => {
+      console.warn("getting data")
       console.warn(data);
+      console.warn("tried to console log data")
       this.articles = data.results;
     });
   }
